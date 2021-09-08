ac411cf15e91580427970f82730603db7a6c4dd8
Natália Alpino <nathyalpino1@gmail.com>
2021-09-07 21:51:36 -0300

    create migration catalog

---
+++ src/backend/database/migrations/20210908001920-catalog.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    
+    await queryInterface.createTable('catalog', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,  
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      unit_value: {
+        type: Sequelize.DECIMAL(10,2),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },      
+    });
+     
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    
+    await queryInterface.dropTable('catalog');
+     
+  }
+};
