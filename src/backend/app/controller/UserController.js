e2874a5ff0875c441802a928ff70606a19f87ef4
RBianchi-Al <renatalebianchi@gmail.com>
2021-09-09 10:00:21 -0300

    create controller user-authenticateJWT

--- src/backend/app/controller/UserController.js
+++ src/backend/app/controller/UserController.js
@@ -1,8 +1,88 @@
+import * as Yup from 'yup';
+import User from "../model/User";
+
 class UserController {
-  async getByIndex(req, res) {
-    // fazer a função aqui
+
+  async store(req, res) {
+    // const schema = Yup.object().shape({
+    //   name: Yup.string().required(),
+    //   email: Yup.string().email().required(),
+    //   password: Yup.string().required().min(6),
+    //   cpf: Yup.string().required().min(11),
+    //   birth_date: Yup.date().required(),
+    //   contact: Yup.string().required(),
+    //   postal_code: Yup.string().required(),
+    //   state: Yup.array().min(27),
+    //   city: Yup.string().required(),
+    //   district: Yup.string().required(),
+    //   number_house: Yup.string().required(),
+    //   street: Yup.string().required(),
+    //   complement: Yup.string().required()
+    // });
+
+    //buscar do body para saber se tem algum problema
+
+    // if (!(await schema.isValid(req.body))) {
+    //   return res.status(401).json({
+    //     message: "Dados inválidos"
+    //   });
+    // }
+
+    const userExists = await User.findOne({
+      where: {
+        cpf: req.body.cpf
+      }
+    })
+
+    if (userExists) {
+      return res.status(401).json({
+        message: "Usuario já existe em nossa base de dados"
+      })
+    }
+
+    const { 
+      id,
+      name,
+      email,
+      cpf,
+      birth_date,
+      contact,
+      postal_code,
+      state,
+      city,
+      district,
+      number_house,
+      street,
+      complement } = await User.create(req.body)
+      	
+    return res.json({
+      id,
+      name,
+      email,
+      cpf,
+      birth_date,
+      contact,
+      postal_code,
+      state,
+      city,
+      district,
+      number_house,
+      street,
+      complement
+    });
+
+  };
+  async index(req, res) {
+    const person = [{
+      name: "Marciel",
+      age: 21
+    },
+    {
+      name: "Renata",
+      age: 21
+    }]
+    return res.status(200).json(person);
   }
-  // inserir os métodos aqui
 }
 
 export default new UserController();
--- src/backend/routes.js
+++ src/backend/routes.js
@@ -1,11 +1,19 @@
 // Importa os arquivos da pasta controller e define os tipos de rota
 
 import { Router } from 'express';
+import AuthController from './app/controller/AuthController';
 
 import UserController from './app/controller/UserController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', UserController.getByIndex);
+routes.post('/user', UserController.store);
+
+routes.post('/auth', AuthController.store);
+
+// routes.use(authMiddleware)
+// routes.get('/auth', UserController.index);
+
 
 export default routes;
