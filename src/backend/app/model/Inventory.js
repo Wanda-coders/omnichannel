53cb3149f23cfb530f0384788e37658f5f9f8dd5
Vanessa de Sousa Araujo <araujovanne@gmail.com>
2021-09-08 22:10:49 -0300

    create model inventory

---
+++ src/backend/app/model/Inventory.js
@@ -0,0 +1,19 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Inventory extends Model {
+  static init(sequelize) {
+    super.init({
+        quantity: Sequelize.INTEGER,
+    }, 
+    {  
+      sequelize, 
+    });
+    return this;
+  }
+  static associate(models){
+    this.belongsTo( models.Catalog, { foreignKey: 'catalog_id', as: 'catalog'})
+    this.belongsTo( models.Store, { foreignKey: 'store_id', as: 'stores'})
+  }
+}
+
+export default Inventory
