a5e80d06aa65b781fb51018efced6a148fcc108c
RBianchi-Al <renatalebianchi@gmail.com>
2021-09-09 16:07:18 -0300

    create controllers-users

--- src/backend/app/controller/UserController.js
+++ src/backend/app/controller/UserController.js
@@ -1,32 +1,8 @@
-import * as Yup from 'yup';
 import User from "../model/User";
 
 class UserController {
 
   async store(req, res) {
-    // const schema = Yup.object().shape({
-    //   name: Yup.string().required(),
-    //   email: Yup.string().email().required(),
-    //   password: Yup.string().required().min(6),
-    //   cpf: Yup.string().required().min(11),
-    //   birth_date: Yup.date().required(),
-    //   contact: Yup.string().required(),
-    //   postal_code: Yup.string().required(),
-    //   state: Yup.array().min(27),
-    //   city: Yup.string().required(),
-    //   district: Yup.string().required(),
-    //   number_house: Yup.string().required(),
-    //   street: Yup.string().required(),
-    //   complement: Yup.string().required()
-    // });
-
-    //buscar do body para saber se tem algum problema
-
-    // if (!(await schema.isValid(req.body))) {
-    //   return res.status(401).json({
-    //     message: "Dados inválidos"
-    //   });
-    // }
 
     const userExists = await User.findOne({
       where: {
