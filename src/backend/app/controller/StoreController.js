65fe2aef794238089ac84dd61f203e2a93ff66b1
Audrey Pujol <audrey.pujol.machado@gmail.com>
2021-09-09 22:52:08 -0300

    Create controller store but with bugs

---
+++ src/backend/app/controller/StoreController.js
@@ -0,0 +1,81 @@
+import * as Yup from 'yup';
+import Store from '../model/Store'
+
+class StoreController{
+  async postStore(req, res) {
+
+    const storeExists = await Store.findOne({
+       where: { 
+         name: req.body.name 
+        }
+      });
+
+    if(storeExists){
+      return res.status(401).json({ message: 'Loja já cadastrada em nossa base' })
+    };
+
+    const { id, name, contact, postal_code, state, city, district, address, number, complement } = await Store.create(req.body);
+    return res.json({id, name, contact, postal_code, state, city, district, address, number, complement});
+  };
+
+  async index(req, res){
+    const person = {
+      name: "Nome da Pessoa",
+      age: 21
+    }
+    return res.status(200).json(person);
+  };
+  async delete(req, res){
+    return res.status(200).json({ message: 'Isso aí psiti!'});
+  };
+  async update(req, res){
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string().when('oldPassword',
+        (oldPassword, field) => oldPassword ? field.required().min(6) : field
+      ),
+      confirmPassword: Yup.string().when('password',
+        (password, field) => password ? field.required().min(6).oneOf([Yup.ref('password')]) : field
+      ),
+    })
+
+    if(!(await schema.isValid(req.body))){
+      return res.status(401).json({ 
+        message: 'Falha na validação'
+      })
+    }
+
+    // console.log(req.userEmail)
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId)
+    console.log('email informado no banco', user.email)
+    console.log('email informado no body', email)
+
+    if(email !== user.email){
+      const userExists = await User.findOne({ where: { email }})
+      // retorno
+      if(userExists){
+        return res.status(400).json({ message: 'Verifique o email informado'})
+      }
+      return res.status(400).json({ message: 'Email não confere'})
+    }
+
+    if(oldPassword && !(await user.checkPassword(oldPassword))){
+      return res.status(400).json({ message: 'Senha não confere'})
+    }
+
+    const { id, name, employee} = await user.update(req.body);
+
+    return res.status(200).json({
+      id, 
+      name, 
+      employee
+    });
+  };
+}
+
+export default new StoreController();
--- src/backend/routes.js
+++ src/backend/routes.js
@@ -6,6 +6,8 @@ import AuthController from './app/controller/AuthController';
 import UserController from './app/controller/UserController';
 import authMiddleware from './app/middlewares/auth';
 
+import StoreController from './app/controller/StoreController';
+
 const routes = new Router();
 
 routes.post('/user', UserController.postClient);
@@ -18,5 +20,31 @@ routes.post('/auth', AuthController.store);
 // routes.use(authMiddleware)
 // routes.get('/auth', UserController.index);
 
+routes.post('/store', StoreController.postStore);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
+// produto
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
+routes.delete('/store/:id', UserController.getById);
+routes.put('/store/:id', UserController.getById);
+// pedido
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
+routes.delete('/store/:id', UserController.getById);
+// estoque
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
+// fotos
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
+// catálogo
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
 
 export default routes;
