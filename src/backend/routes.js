5db621a8394053822d487e4d767d38843d5b7e6e
RBianchi-Al <renatalebianchi@gmail.com>
2021-09-09 22:36:23 -0300

    create route update user

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -7,16 +7,49 @@ import UserController from './app/controller/UserController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
-
+// users
 routes.post('/user', UserController.postClient);
 routes.get('/user', UserController.getAllClients);
 routes.get('/user/:id', UserController.getById);
-// routes.put('/user/:id', UserController.updateById);
+routes.put('/user', UserController.updateById);
 
 routes.post('/auth', AuthController.store);
 
 // routes.use(authMiddleware)
 // routes.get('/auth', UserController.index);
+// store
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
+
+
+// produto
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
+routes.delete('/store/:id', UserController.getById);
+routes.put('/store/:id', UserController.getById);
+
+// pedido
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
+routes.delete('/store/:id', UserController.getById);
+
+// estoque
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
+
+// fotos
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
+
+// catálogo
+routes.post('/store', UserController.postClient);
+routes.get('/store', UserController.getAllClients);
+routes.get('/store/:id', UserController.getById);
 
 
 export default routes;
