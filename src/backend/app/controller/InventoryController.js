f3064377d4fa0e07dbdc9aa4e0fb7da6e2d988a2
Audrey Pujol <audrey.pujol.machado@gmail.com>
2021-09-12 14:57:51 -0300

    Filter results in Inventory with quantity > 0

--- src/backend/app/controller/InventoryController.js
+++ src/backend/app/controller/InventoryController.js
@@ -5,18 +5,18 @@ class InventoryController {
 
   async postInventory(req, res) {
 
+    const { quantity } = req.body
+    if (quantity < 0) {
+      return res.status(401).json({
+        message: "Quantity cannot be negative!"
+      })
+    }
     const inventoryExists = await Inventory.findOne({
       where: {
         catalog_id: req.body.catalog_id,
         store_id: req.body.store_id
       }
     })
-    const { quantity } = req.body
-    if (quantity <= 0) {
-      return res.status(401).json({
-        message: "Quantity must be bigger than zero!"
-      })
-    }
     if (inventoryExists) {
       return res.status(401).json({
         message: "Inventory already exists!"
@@ -39,12 +39,8 @@ class InventoryController {
 
   async getInventory(req, res) {
     const { catalog_id, store_id } = req.query;
-    console.log(catalog_id)
-    console.log(store_id)
-
     let inventoryResults;
     if ((catalog_id !== undefined) & (store_id !== undefined)) {
-      console.log("Filter by catalog and store")
       inventoryResults = await Inventory.findAll({
         where: {
           catalog_id: catalog_id,
@@ -53,7 +49,6 @@ class InventoryController {
       })
     }
     else if (catalog_id !== undefined) {
-      console.log("Filter by catalog only")
       inventoryResults = await Inventory.findAll({
         where: {
           catalog_id: catalog_id,
@@ -61,7 +56,6 @@ class InventoryController {
       })
     }
     else if (store_id !== undefined) {
-      console.log("Filter by store only")
       inventoryResults = await Inventory.findAll({
         where: {
           store_id: store_id,
@@ -69,9 +63,9 @@ class InventoryController {
       })
     }
     else {
-      console.log("No filter")
       inventoryResults = await Inventory.findAll()
     }
+    inventoryResults = inventoryResults.filter((element) => element.quantity > 0)
     if (!inventoryResults) {
       return res.status(404).json({
         message: "Inventory not found!"
