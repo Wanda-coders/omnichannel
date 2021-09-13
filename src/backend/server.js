25d973531e17abf0e5fa70a6074eee683bf4cd25
Audrey Pujol <audrey.pujol.machado@gmail.com>
2021-09-13 16:01:56 -0300

    Use node server on Procfile

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,6 +4,7 @@ const swaggerFile = require('../../swagger_output.json')
 
 const PORT = process.env.PORT;
 const HOST = 'localhost'
+console.log(`Starting to listen on port ${PORT}`);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.listen(PORT);
 console.log(`Online on port http://${HOST}:${PORT}/docs`);
